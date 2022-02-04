77cbc2aede604a7ea8d1996bc59a7a615b46ed9f
Rufus Drew <rufusdrew3@gmail.com>
2022-02-04 14:11:49 +0530

    First Commit

---
+++ src/components/Quotes.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+
+const Quotes = () => {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
+  const getQuote = () => {
+    let url =
+      "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        let dataQuotes = data.quotes;
+        let randomNum = Math.floor(Math.random() * dataQuotes.length);
+        let randomQuotes = dataQuotes[randomNum];
+        setQuote(randomQuotes.quote);
+        setAuthor(randomQuotes.author);
+      });
+  };
+
+  const handleClick = () => {
+    getQuote();
+  };
+
+  return (
+    <div id="quote-box">
+      <div id="text">
+        <p>{quote}</p>
+      </div>
+      <div id="author">
+        <p>{author}</p>
+      </div>
+
+      <div id="buttons">
+        <div className="social-media">
+          <a href="#" id="twet-quote">
+            <span>
+              <img
+                src="https://img.icons8.com/ios-glyphs/30/000000/twitter--v1.png"
+                alt=""
+              />
+            </span>
+          </a>
+          <a href="#" id="tumlr-quote">
+            <span>
+              <img
+                src="https://img.icons8.com/ios-glyphs/30/000000/facebook-new.png"
+                alt=""
+              />
+            </span>
+          </a>
+        </div>
+        <button onClick={handleClick} id="new-quote">
+          New quote
+        </button>
+      </div>
+      <div id="text2">
+        <p>Developed By Kristyn</p>
+      </div>
+    </div>
+  );
+};
+
+export default Quotes;
